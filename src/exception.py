2a0e7671a84b6fae3078fafadc54d26436f71738
shukla-2005 <araj84935@gmail.com>
2026-08-15 11:46:45 +0530

    notebook commit

--- src/exception.py
+++ src/exception.py
@@ -16,4 +16,4 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message
+        return self.error_message   
